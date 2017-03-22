709da49723969caea986341ec1028b202b20a5da
Christina <cdhorn515@gmail.com>
2017-03-21 21:08:28 -0400

    created body and navbar files

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import NavBar from './NavBar';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <NavBar />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/NavBar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './NavBar.css';
+
+class NavBar extends Component {
+	render() {
+		return (
+			<div className="navbar">
+				Christina Cornell
+			</div>
+		);
+	}
+}
+
+export default NavBar;
